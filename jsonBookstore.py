7680a0ea24e9a43e55ff3330959c5504205ccb5d
rmedcraft <rwtmedcraft@gmail.com>
2024-10-17 00:23:16 -0400

    started json

--- jsonBookstore.py
+++ jsonBookstore.py
@@ -1,4 +1,18 @@
+import json
+
+with open("bookstore.json", "r") as file:
+    root = json.load(file)
+
+print(root)
+
 # Titles of the books followed by Year
+for book in root:
+    print(book)
+    for child in root[str(book)]:
+        print(child) # just prints the keys???
+
+# for i in root["book1"]:
+#     print(i)
 
 # Titles of the books followed by all authors
 
